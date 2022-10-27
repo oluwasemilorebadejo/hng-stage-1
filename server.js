11b1341d6d0efc91c42f04dc274184c5928ad4d6
Oluwasemilore Badejo <badejo.semilore.7@gmail.com>
2022-10-27 16:02:44 +0100

    make prod ready

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const dotenv = require("dotenv");
 
+dotenv.config({ path: "./config.env" }); // has to be set before requiring app because the env vars have to be set so the app module has access to it also
+
 const app = require("./app");
 
-const server = app.listen(8000, () => {
+port = process.env.PORT;
+const server = app.listen(port, () => {
   console.log(`listening on port 8000`);
 });
