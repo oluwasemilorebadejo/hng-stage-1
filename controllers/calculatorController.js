b43259f9fda384cc81e037a68ffe66688e79a162
Oluwasemilore Badejo <badejo.semilore.7@gmail.com>
2022-11-04 20:51:25 +0100

    change to operation_type

--- controllers/calculatorController.js
+++ controllers/calculatorController.js
@@ -1,20 +1,22 @@
 exports.calculate = async (req, res) => {
   x = req.body.x * 1;
   y = req.body.y * 1;
-  if (req.body.operationType === "addition") {
+  if (req.body.operation_type === "addition") {
     result = x + y;
-  } else if (req.body.operationType === "subtraction") {
+  } else if (req.body.operation_type === "subtraction") {
     result = x - y;
-  } else if (req.body.operationType === "multiplication") {
+  } else if (req.body.operation_type === "multiplication") {
     result = x * y;
   } else {
     result =
       "WRONG OPERATION TYPE! Kindly pick between addition, subtraction or multiplication";
   }
 
+  console.log(req.body);
+
   res.status(200).json({
     slackUsername: "oluwasemilore",
     result: result,
-    operation_type: req.body.operationType,
+    operation_type: req.body.operation_type,
   });
 };
